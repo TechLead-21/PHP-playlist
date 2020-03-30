613488165acef2587e9d41c17ef1c88880742bcd
Jayesh Tembhekar <jtembhekar2@gmail.com>
2020-03-30 15:10:56 +0530

    Added count function

---
+++ count.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+               The count() Function
+						   
+The count() function is used to return the length (the number of elements) of an array.
+
+*/
+
+
+$fruits = array( "apple" , "strawberry" , "blueberry" , "banana" , "orange" , "pineapple" );
+$name = array("John" => "Smith" , "Lucy" => "Williams" , "Kitty" => "Smith" , "Luke" => "Cage");
+
+
+echo count($name);
+
+?>
