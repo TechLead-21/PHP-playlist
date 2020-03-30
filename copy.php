a1d07f1c923410006ca273fb665775bba9033ba9
Jayesh Tembhekar <jtembhekar2@gmail.com>
2020-03-30 15:10:33 +0530

    Added copy in php

---
+++ copy.php
@@ -0,0 +1,26 @@
+<?php 
+
+/*
+                        PHP copy() Function
+								   
+The copy() function copies a file.
+
+This function returns TRUE on success and FALSE on failure.
+
+Syntax:
+
+copy(file,to_file);
+
+Parameter	            Description
+
+file	      Required. Specifies the file to copy.
+to_file       Required. Specifies the file to copy to.
+
+
+*/
+
+
+copy("x123.txt" , "abc.txt");
+
+
+?>
